f6c81cb78eafdcbefc8e93ff6b97a7450fc0509b
unknown <alexandramacedo96@gmail.com>
2024-02-18 17:51:09 -0500

    (feat:) contructores pelicula

--- Objetos/Pelicula.java
+++ Objetos/Pelicula.java
@@ -3,6 +3,9 @@ package Objetos;
 public class Pelicula {
     private String titulo;
     private int duracion;
-
+    public Pelicula(String titulo, int duracion) {
+        this.titulo = titulo;
+        this.duracion = duracion;
+    }
 
 }
