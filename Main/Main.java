0e64fc19db6788df5228363d13ecd7208541ed69
unknown <alexandramacedo96@gmail.com>
2024-02-18 17:32:11 -0500

    (feat:) importaciones iniciales

---
+++ Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Cine cine = new Cine("Cine Cinesa", 500);
+        cine.inicializarSalas();
+    }
+}
