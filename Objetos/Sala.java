d342290d215a8cf8b068202ba2dac54a51326a0d
unknown <alexandramacedo96@gmail.com>
2024-02-18 17:55:31 -0500

    (feat:) contructores sala

--- Objetos/Sala.java
+++ Objetos/Sala.java
@@ -5,4 +5,11 @@ public class Sala {
     private int numSala;
     private String[][] butacas;
     private Pelicula pelicula;
+
+    public Sala(int numSala, int filas, int columnas, Pelicula pelicula) {
+        this.numSala = numSala;
+        this.butacas = new String[filas][columnas];
+        this.pelicula = pelicula;
+
+    }
 }
