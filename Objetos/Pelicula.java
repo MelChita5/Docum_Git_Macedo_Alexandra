65aa80959d8b125f284e4e1522d4e7f77c387f89
unknown <alexandramacedo96@gmail.com>
2024-02-18 17:51:56 -0500

    (perf:) getters 7/ setters

--- Objetos/Pelicula.java
+++ Objetos/Pelicula.java
@@ -8,4 +8,21 @@ public class Pelicula {
         this.duracion = duracion;
     }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
 }
