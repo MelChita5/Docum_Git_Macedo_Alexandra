45f050a557ecf6552bb009d4cc6864a0c5b05b9a
unknown <alexandramacedo96@gmail.com>
2024-02-18 18:04:40 -0500

    (perf:) setter y getters

--- Objetos/Cine.java
+++ Objetos/Cine.java
@@ -11,4 +11,28 @@ public class Cine {
         this.aforo = aforo;
         this.salas = new Sala[5];
     }
+
+    public String getNombreCine() {
+        return nombreCine;
+    }
+
+    public void setNombreCine(String nombreCine) {
+        this.nombreCine = nombreCine;
+    }
+
+    public int getAforo() {
+        return aforo;
+    }
+
+    public void setAforo(int aforo) {
+        this.aforo = aforo;
+    }
+
+    public Sala[] getSalas() {
+        return salas;
+    }
+
+    public void setSalas(Sala[] salas) {
+        this.salas = salas;
+    }
 }
