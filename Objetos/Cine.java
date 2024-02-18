9b4154a00b7d9813fcf6d6017f8adbb6e1328b61
unknown <alexandramacedo96@gmail.com>
2024-02-18 18:03:49 -0500

    (feat:) contructor cine

--- Objetos/Cine.java
+++ Objetos/Cine.java
@@ -5,4 +5,10 @@ public class Cine {
     private String nombreCine;
     private int aforo;
     private Sala[] salas;
+
+    public Cine(String nombreCine, int aforo) {
+        this.nombreCine = nombreCine;
+        this.aforo = aforo;
+        this.salas = new Sala[5];
+    }
 }
