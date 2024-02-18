5ca0abeb110511fbe2452486709d54042dcef52f
unknown <alexandramacedo96@gmail.com>
2024-02-18 17:44:07 -0500

    (fix:) bucle arreglado y agregado

--- Main/Main.java
+++ Main/Main.java
@@ -8,54 +8,59 @@ public class Main {
         Cine cine = new Cine("Cine Cinesa", 500);
         cine.inicializarSalas();
 
-        System.out.println(
-                "Bienvenidos a " + cine.getNombreCine() + "\n" +
-                        "1.- Ver Películas en Cartelera\n" +
-                        "2.- Comprar Entrada\n" +
-                        "3.- Salir"
-        );
+        int opcion = 0;
 
-        int opcion1 = scanner.nextInt();
+        while (opcion != 3) {
+            System.out.println(
+                    "Bienvenidos a " + cine.getNombreCine() + "\n" +
+                            "1.- Ver Películas en Cartelera\n" +
+                            "2.- Comprar Entrada\n" +
+                            "3.- Salir"
+            );
 
-        switch (opcion1){
-            case 1:
-                cine.mostrarCartelera();
-                break;
-            case 2:
-                System.out.print("Ingrese el número de sala (1 - 5): ");
-                int numeroSala = scanner.nextInt();
+            int opcion1 = scanner.nextInt();
 
-                if (numeroSala >= 1 && numeroSala <= cine.getSalas().length){
+            switch (opcion1){
+                case 1:
+                    cine.mostrarCartelera();
+                    break;
+                case 2:
+                    System.out.print("Ingrese el número de sala (1 - 5): ");
+                    int numeroSala = scanner.nextInt();
 
-                    Sala salaSeleccionada = cine.getSalas()[numeroSala - 1];
+                    if (numeroSala >= 1 && numeroSala <= cine.getSalas().length){
+
+                        Sala salaSeleccionada = cine.getSalas()[numeroSala - 1];
 
 
-                    salaSeleccionada.mostrarInfo();
+                        salaSeleccionada.mostrarInfo();
 
 
-                    System.out.print("Ingrese la fila (1-5): ");
-                    int fila = scanner.nextInt();
+                        System.out.print("Ingrese la fila (1-5): ");
+                        int fila = scanner.nextInt();
 
-                    System.out.print("Ingrese la columna (1-10): ");
-                    int columna = scanner.nextInt();
-                    if (salaSeleccionada.reservarButaca(fila, columna)) {
-                        System.out.println("Reserva realizada :D");
-                        salaSeleccionada.mostrarButacas();
+                        System.out.print("Ingrese la columna (1-10): ");
+                        int columna = scanner.nextInt();
+                        if (salaSeleccionada.reservarButaca(fila, columna)) {
+                            System.out.println("Reserva realizada :D");
+                            salaSeleccionada.mostrarButacas();
+                        } else {
+                            System.out.println("La butaca no esta disponible.");
+                        }
                     } else {
-                        System.out.println("La butaca no esta disponible.");
+                        System.out.println("ERROR, numero de sala no valido.");
                     }
-                } else {
-                    System.out.println("ERROR, numero de sala no valido.");
-                }
-                break;
+                    break;
 
-            case 3:
-                System.out.println("Saliendo...");
-                break;
-            default:
-                System.out.println("Saliendo...");
-                break;
+                case 3:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Saliendo...");
+                    break;
 
+            }
         }
+
     }
 }
